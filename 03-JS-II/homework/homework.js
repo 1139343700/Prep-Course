18f441ad8447225e7bdb326ad89f784b58b0c310
Sancho <santiago@taxes.com.ar>
2022-04-15 17:12:25 -0300

    homework 3, 6 and 8

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -38,12 +38,27 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+switch (color) {
+  case "blue":
+    return "This is blue";
+  case "red":
+    return "This is red";
+  case "green":
+    return "This is green";
+  case "orange":
+    return "This is orange";
+  default:
+    return "Color not found"
+
+}
+
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero === 5 || numero === 10
 }
 
 function estaEnRango(numero) {
@@ -84,7 +99,18 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero <= 0){
+    return false
+  }
+  for (let i = 2; i < numero; i++) {
+    if ( numero%i===0){
+      return false;
+    }
+    return true
+  }
+
 }
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
@@ -97,19 +123,39 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  var array = [];
+
+  for (let i = 0; i < 11; i++) {
+        array.push(6 * i)
+  }
+  return array
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  var string = numero.toString();
   
+  if (string.length === 3) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+
+  var a = numero;
+  var i = 0;
+  do {
+    i = i + 1;
+    a = a + 5;
+  }
+  while(i < 8);
+  return a;
 }
 
 
